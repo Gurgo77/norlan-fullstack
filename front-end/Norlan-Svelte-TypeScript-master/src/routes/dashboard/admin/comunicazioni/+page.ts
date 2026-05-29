6a8c1f2ba9e275998fefa88a1cd3306911db186f
nicolobaldari <nicolobaldari@gmail.com>
2026-05-29 18:17:17 +0200

    Eliminato +page.ts da cartella comunicazioni in dashboard admin

--- front-end/Norlan-Svelte-TypeScript-master/src/routes/dashboard/admin/comunicazioni/+page.ts
+++
@@ -1,7 +0,0 @@
-/*
-Disabilita il Rendering lato Server (SSR) per questa pagina o layout,
-forzando il rendering esclusivamente lato client (CSR).
-Utile per componenti che dipendono pesantemente da browser-APIs (es. window, localStorage, WebSocket)
-o per dashboard protette dove la generazione server-side non è necessaria.
-*/
-export const ssr = false;
